f402166124432ba1c5798c34383ca09be059317c
hugojavid5 <hugojavidferrero@gmail.com>
2024-11-03 18:39:09 +0100

    modificada la calse controller añadiendo la clase temporizador del ejercicio q

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module org.hugo.ejercicior {
-    requires javafx.controls;
+
     requires javafx.fxml;
+
+    requires org.hugo.ejercicioq;
+    requires javafx.controls;
+    requires java.desktop;
 
     opens org.hugo.ejercicior to javafx.fxml;
     exports org.hugo.ejercicior;
--- src/main/java/org/hugo/ejercicior/FormularioController.java
+++ src/main/java/org/hugo/ejercicior/FormularioController.java
@@ -1,10 +1,15 @@
 package org.hugo.ejercicior;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+
+import javafx.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.Alert;
 
 public class FormularioController implements Initializable {
     @FXML
@@ -14,11 +19,47 @@ public class FormularioController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //
+        if (temporizador.setSegundos(20)) {
+            temporizador.iniciar();
+            temporizador.finProperty().addListener(new ChangeListener<Object>() {
+                @Override
+                public void changed(ObservableValue<? extends Object> arg0, Object arg1, Object arg2) {
+                    //código con las instrucciones a realizar
+                    deshabilitarBotones();
+                }
+            });
+        } else {
+            deshabilitarBotones();
+            alerta("No se ha podido iniciar el temporizador");
+        }
     }
 
     public void deshabilitarBotones() {
         btnEnviar1.setDisable(true);
         btnEnviar2.setDisable(true);
     }
+    @FXML
+    void enviar(ActionEvent event) {
+        confirmacion("Enviado");
+    }
+    public void alerta(String texto) {
+        Alert alerta = new Alert(Alert.AlertType.ERROR);
+        alerta.setHeaderText(null);
+        alerta.setTitle("Error");
+        alerta.setContentText(texto);
+        alerta.showAndWait();
+    }
+    /**
+     * Función que muestra un mensaje de confirmación al usuario
+     *
+     * @param texto contenido del mensaje
+     */
+    public void confirmacion(String texto) {
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setHeaderText(null);
+        alerta.setTitle("Info");
+        alerta.setContentText(texto);
+        alerta.showAndWait();
+    }
+
 }
